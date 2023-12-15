71286e3b86346cb3c56a4baba243fb7dc7dea10a
Silvia Ocegueda <margarita.ocegueda@gmail.com>
2023-12-15 06:42:39 -0700

    Fix OneInchEIP712Visualizer test

--- test/OneInchEIP712Visualizer.test.js
+++ test/OneInchEIP712Visualizer.test.js
@@ -1,5 +1,6 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { json } = require("hardhat/internal/core/params/argumentTypes");
 
 describe("OneInchEIP712Visualizer", function () {
   let OneInchEIP712Visualizer;
@@ -25,7 +26,7 @@ describe("OneInchEIP712Visualizer", function () {
   const encodedMessage = lengthPadded + encodedWitouthLength.slice(2); // Slice off the '0x' prefix
   const expectedInitialRateBump = ethers.BigNumber.from("50000");
   const expectedTakingAmount = ethers.BigNumber.from("1420000000");
-  const expectedMaxTakingAmount = expectedTakingAmount * (10000000 + expectedInitialRateBump) / 10000000;
+  const expectedMaxTakingAmount = expectedTakingAmount.mul(ethers.BigNumber.from("10000000").add(expectedInitialRateBump)).div(ethers.BigNumber.from("10000000"));
   
   beforeEach(async function () {
     const OneInchEIP712VisualizerFactory = await ethers.getContractFactory("OneInchEIP712Visualizer");
@@ -33,45 +34,30 @@ describe("OneInchEIP712Visualizer", function () {
     await OneInchEIP712Visualizer.deployed();
   });
 
-  it("Should visualize EIP712 message", async function () {
+  it("Should return the correct values when calling visualizeEIP712message", async function () {
 
-    const expectedReturnValue = {
-      assetsIn: {
-        assetTokenAddress: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-        id: ethers.BigNumber.from("0"),
-        amounts: [expectedMaxTakingAmount, expectedTakingAmount]
-      },
-      assetsOut: {
-        assetTokenAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-        id: ethers.BigNumber.from("0"),
-        amounts: [ethers.BigNumber.from("1000000000000000000")]
-      },
-      liveness: {
-          from: ethers.BigNumber.from("1673548149"),
-          to: ethers.BigNumber.from("1673548329")
-      }
-    };
-    // const expectedReturnValue = [
-    //   {
-    //     assetTokenAddress: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-    //     id: ethers.BigNumber.from("0"),
-    //     amounts: [expectedMaxTakingAmount, expectedTakingAmount]
-    //   },
-    //   {
-    //     assetTokenAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-    //     id: ethers.BigNumber.from("0"),
-    //     amounts: [ethers.BigNumber.from("1000000000000000000")]
-    //   },
-    //   {
-    //     from: ethers.BigNumber.from("1673548149"),
-    //     to: ethers.BigNumber.from("1673548329")
-    //   }
-    // ];
-    console.log("Expected: ", expectedReturnValue)
+    const expectedAssetsInassetTokenAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+    const expectedAssetsInId = ethers.BigNumber.from("0");
+    const expectedAssetsInAmounts = [expectedMaxTakingAmount, expectedTakingAmount];
+    const expectedAssetsOutAssetTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+    const expectedAssetsOutId = ethers.BigNumber.from("0");
+    const expectedAssetsOutAmounts = [ethers.BigNumber.from("1000000000000000000")];
+    const expectedLivenessFrom = ethers.BigNumber.from("1673548149");
+    const expectedLivenessTo = ethers.BigNumber.from("1673548329");
+    
    
     const result = await OneInchEIP712Visualizer.visualizeEIP712Message(encodedMessage, DOMAIN_SEPARATOR);
-    console.log("Result: ", result)
-    expect(result).to.deep.equal(expectedReturnValue);
+   
+    expect(result.assetsIn[0].assetTokenAddress).to.equal(expectedAssetsInassetTokenAddress);
+    expect(result.assetsIn[0].id).to.equal(expectedAssetsInId);
+    expect(result.assetsIn[0].amounts[0]).to.equal(expectedAssetsInAmounts[0]);
+    expect(result.assetsIn[0].amounts[1]).to.equal(expectedAssetsInAmounts[1]);
+    expect(result.assetsOut[0].assetTokenAddress).to.equal(expectedAssetsOutAssetTokenAddress);
+    expect(result.assetsOut[0].id).to.equal(expectedAssetsOutId);
+    expect(result.assetsOut[0].amounts[0]).to.equal(expectedAssetsOutAmounts[0]);
+    expect(result.assetsOut[0].amounts[1]).to.be.undefined;
+    expect(result.liveness.from).to.equal(expectedLivenessFrom);
+    expect(result.liveness.to).to.equal(expectedLivenessTo);
   });
   it("Should revert when visualizing EIP712 message due to incorrect domain separator", async function () {
     const INCORRECT_DOMAIN_SEPARATOR = "0x36c25de3e541d5d970f66e4210d728721220fff5c077cc6cd008b3a0c62adab7";
