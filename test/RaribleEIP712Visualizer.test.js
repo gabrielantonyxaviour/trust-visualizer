7b58d6260c3c0a74bf241716f6be3e780ce753e2
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2023-12-16 22:06:06 +0530

    add 2 more test cases

--- test/RaribleEIP712Visualizer.test.js
+++ test/RaribleEIP712Visualizer.test.js
@@ -1,104 +1,16 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-
+const {
+  types,
+  orderERC20InERC1155Out,
+  orderErc1155InEthOut,
+  orderETHInERC721Out,
+} = require("../utils/raribleData");
 describe("RaribleEIP712Visualizer", function () {
   let RaribleEIP712Visualizer;
   const DOMAIN_SEPARATOR =
     "0x36c25de3e541d5d970f66e4210d728721220fff5c077cc6cd008b3a0c62adab7";
-  const types = [
-    "address",
-    {
-      type: "tuple",
-      name: "makerAsset",
-      components: [
-        {
-          name: "assetType",
-          type: "tuple",
-          components: [
-            { name: "assetClass", type: "bytes4" },
-            { name: "data", type: "bytes" },
-          ],
-        },
-        { name: "value", type: "uint256" },
-      ],
-    },
-    "address",
-    {
-      type: "tuple",
-      name: "takerAsset",
-      components: [
-        {
-          name: "assetType",
-          type: "tuple",
-          components: [
-            { name: "assetClass", type: "bytes4" },
-            { name: "data", type: "bytes" },
-          ],
-        },
-        { name: "value", type: "uint256" },
-      ],
-    },
-    "uint256",
-    "uint256",
-    "uint256",
-    "bytes4",
-    "bytes",
-  ];
-  const orderErc1155InEthOutStruct = {
-    maker: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", // replace with actual address
-    makeAsset: {
-      assetType: {
-        assetClass: ethers.utils
-          .keccak256(ethers.utils.toUtf8Bytes("ETH"))
-          .slice(0, 10), // replace with actual bytes4
-        data: "0x00", // replace with actual bytes
-      },
-      value: "1000000000000000000",
-    },
-    taker: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", // replace with actual address
-    takeAsset: {
-      assetType: {
-        assetClass: ethers.utils
-          .keccak256(ethers.utils.toUtf8Bytes("ERC1155"))
-          .slice(0, 10), // replace with actual bytes4
-        data: ethers.utils.defaultAbiCoder.encode(
-          ["address", "uint256"],
-          ["0x00000000219ab540356cBB839Cbe05303d7705Fa", 2]
-        ), // replace with actual bytes
-      },
-      value: "8",
-    },
-    salt: ethers.BigNumber.from(
-      "45118768841948961586167738353692277076075522015101619148498725069326976558864"
-    ),
-    start: ethers.BigNumber.from("1698643839"),
-    end: ethers.BigNumber.from("1798843839"),
-    dataType: "0x12345678",
-    data: "0x00",
-  };
-  const orderErc1155InEthOut = [
-    orderErc1155InEthOutStruct.maker,
-    orderErc1155InEthOutStruct.makeAsset,
-    orderErc1155InEthOutStruct.taker,
-    orderErc1155InEthOutStruct.takeAsset,
-    orderErc1155InEthOutStruct.salt,
-    orderErc1155InEthOutStruct.start,
-    orderErc1155InEthOutStruct.end,
-    orderErc1155InEthOutStruct.dataType,
-    orderErc1155InEthOutStruct.data,
-  ];
-  console.log(orderErc1155InEthOut);
-  // To mimic Solidity's abi.encode in ethers.js, manually add the length of the dynamic data, "bytes" in types array,
-  // to the start of the encoded output
-  const encodedWitouthLength = ethers.utils.defaultAbiCoder.encode(
-    types,
-    orderErc1155InEthOut
-  );
-  const lengthPadded =
-    "0x0000000000000000000000000000000000000000000000000000000000000020";
-  const encodedMessage = lengthPadded + encodedWitouthLength.slice(2); // Slice off the '0x' prefix
 
-  console.log(encodedMessage);
   beforeEach(async function () {
     const RaribleEIP712VisualizerFactory = await ethers.getContractFactory(
       "RaribleEIP712Visualizer"
@@ -107,7 +19,33 @@ describe("RaribleEIP712Visualizer", function () {
     await RaribleEIP712Visualizer.deployed();
   });
 
-  it("Should return the correct values when calling visualizeEIP712message", async function () {
+  it("Should revert when visualizing EIP712 message due to incorrect domain separator", async function () {
+    const encodedWithoutLength = ethers.utils.defaultAbiCoder.encode(
+      types,
+      orderErc1155InEthOut
+    );
+    const lengthPadded =
+      "0x0000000000000000000000000000000000000000000000000000000000000020";
+    const encodedMessage = lengthPadded + encodedWithoutLength.slice(2);
+
+    const INCORRECT_DOMAIN_SEPARATOR =
+      "0x96c25de3e541d5d970f66e4210d728721220fff5c077cc6cd008b3a0c62adab7";
+    await expect(
+      RaribleEIP712Visualizer.visualizeEIP712Message(
+        encodedMessage,
+        INCORRECT_DOMAIN_SEPARATOR
+      )
+    ).to.be.revertedWith("RaribleEIP712Visualizer: unsupported domain");
+  });
+
+  it("Should return the correct values when calling visualizeEIP712message to decode ERC1155 In ETH Out Order", async function () {
+    const encodedWithoutLength = ethers.utils.defaultAbiCoder.encode(
+      types,
+      orderErc1155InEthOut
+    );
+    const lengthPadded =
+      "0x0000000000000000000000000000000000000000000000000000000000000020";
+    const encodedMessage = lengthPadded + encodedWithoutLength.slice(2);
     const result = await RaribleEIP712Visualizer.visualizeEIP712Message(
       encodedMessage,
       DOMAIN_SEPARATOR
@@ -154,14 +92,113 @@ describe("RaribleEIP712Visualizer", function () {
 
     expect(mappedResult).to.deep.equal(expectedResult);
   });
-  it("Should revert when visualizing EIP712 message due to incorrect domain separator", async function () {
-    const INCORRECT_DOMAIN_SEPARATOR =
-      "0x96c25de3e541d5d970f66e4210d728721220fff5c077cc6cd008b3a0c62adab7";
-    await expect(
-      RaribleEIP712Visualizer.visualizeEIP712Message(
-        encodedMessage,
-        INCORRECT_DOMAIN_SEPARATOR
-      )
-    ).to.be.revertedWith("RaribleEIP712Visualizer: unsupported domain");
+  it("Should return the correct values when calling visualizeEIP712message to decode ERC20 In ERC1155 Out Order", async function () {
+    const encodedWithoutLength = ethers.utils.defaultAbiCoder.encode(
+      types,
+      orderERC20InERC1155Out
+    );
+    const lengthPadded =
+      "0x0000000000000000000000000000000000000000000000000000000000000020";
+    const encodedMessage = lengthPadded + encodedWithoutLength.slice(2);
+    const result = await RaribleEIP712Visualizer.visualizeEIP712Message(
+      encodedMessage,
+      DOMAIN_SEPARATOR
+    );
+
+    // Map the result to a more readable format
+    const mappedResult = {
+      assetsIn: result.assetsIn.map(([assetTokenAddress, id, amounts]) => ({
+        assetTokenAddress,
+        id,
+        amounts,
+      })),
+      assetsOut: result.assetsOut.map(([assetTokenAddress, id, amounts]) => ({
+        assetTokenAddress,
+        id,
+        amounts,
+      })),
+      liveness: {
+        from: result.liveness[0],
+        to: result.liveness[1],
+      },
+    };
+
+    const expectedResult = {
+      assetsIn: [
+        {
+          assetTokenAddress:
+            "0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8".toLowerCase(),
+          id: ethers.BigNumber.from("0"),
+          amounts: [ethers.BigNumber.from("10000000000000000000")],
+        },
+      ],
+      assetsOut: [
+        {
+          assetTokenAddress: "0x00000000219ab540356cBB839Cbe05303d7705Fa",
+          id: ethers.BigNumber.from("2"),
+          amounts: [ethers.BigNumber.from("6")],
+        },
+      ],
+      liveness: {
+        from: ethers.BigNumber.from("1698643839"),
+        to: ethers.BigNumber.from("1798843839"),
+      },
+    };
+
+    expect(mappedResult).to.deep.equal(expectedResult);
+  });
+  it("Should return the correct values when calling visualizeEIP712message to decode ETH In ERC721 Out Order", async function () {
+    const encodedWithoutLength = ethers.utils.defaultAbiCoder.encode(
+      types,
+      orderETHInERC721Out
+    );
+    const lengthPadded =
+      "0x0000000000000000000000000000000000000000000000000000000000000020";
+    const encodedMessage = lengthPadded + encodedWithoutLength.slice(2);
+    const result = await RaribleEIP712Visualizer.visualizeEIP712Message(
+      encodedMessage,
+      DOMAIN_SEPARATOR
+    );
+
+    // Map the result to a more readable format
+    const mappedResult = {
+      assetsIn: result.assetsIn.map(([assetTokenAddress, id, amounts]) => ({
+        assetTokenAddress,
+        id,
+        amounts,
+      })),
+      assetsOut: result.assetsOut.map(([assetTokenAddress, id, amounts]) => ({
+        assetTokenAddress,
+        id,
+        amounts,
+      })),
+      liveness: {
+        from: result.liveness[0],
+        to: result.liveness[1],
+      },
+    };
+
+    const expectedResult = {
+      assetsIn: [
+        {
+          assetTokenAddress: "0x0000000000000000000000000000000000000000",
+          id: ethers.BigNumber.from("0"),
+          amounts: [ethers.BigNumber.from("100000000000000000")],
+        },
+      ],
+      assetsOut: [
+        {
+          assetTokenAddress: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+          id: ethers.BigNumber.from("10"),
+          amounts: [ethers.BigNumber.from("1")],
+        },
+      ],
+      liveness: {
+        from: ethers.BigNumber.from("1698643839"),
+        to: ethers.BigNumber.from("1798843839"),
+      },
+    };
+
+    expect(mappedResult).to.deep.equal(expectedResult);
   });
 });
---
+++ utils/raribleData.js
@@ -0,0 +1,184 @@
+const { ethers } = require("hardhat");
+
+function getAssetClass(assetType) {
+  return ethers.utils
+    .keccak256(ethers.utils.toUtf8Bytes(assetType))
+    .slice(0, 10);
+}
+
+const types = [
+  "address",
+  {
+    type: "tuple",
+    name: "makerAsset",
+    components: [
+      {
+        name: "assetType",
+        type: "tuple",
+        components: [
+          { name: "assetClass", type: "bytes4" },
+          { name: "data", type: "bytes" },
+        ],
+      },
+      { name: "value", type: "uint256" },
+    ],
+  },
+  "address",
+  {
+    type: "tuple",
+    name: "takerAsset",
+    components: [
+      {
+        name: "assetType",
+        type: "tuple",
+        components: [
+          { name: "assetClass", type: "bytes4" },
+          { name: "data", type: "bytes" },
+        ],
+      },
+      { name: "value", type: "uint256" },
+    ],
+  },
+  "uint256",
+  "uint256",
+  "uint256",
+  "bytes4",
+  "bytes",
+];
+
+const orderErc1155InEthOutStruct = {
+  maker: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", // replace with actual address
+  makeAsset: {
+    assetType: {
+      assetClass: getAssetClass("ETH"), // replace with actual bytes4
+      data: "0x00", // replace with actual bytes
+    },
+    value: "1000000000000000000",
+  },
+  taker: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", // replace with actual address
+  takeAsset: {
+    assetType: {
+      assetClass: getAssetClass("ERC1155"), // replace with actual bytes4
+      data: ethers.utils.defaultAbiCoder.encode(
+        ["address", "uint256"],
+        ["0x00000000219ab540356cBB839Cbe05303d7705Fa", 2]
+      ), // replace with actual bytes
+    },
+    value: "8",
+  },
+  salt: ethers.BigNumber.from(
+    "45118768841948961586167738353692277076075522015101619148498725069326976558864"
+  ),
+  start: ethers.BigNumber.from("1698643839"),
+  end: ethers.BigNumber.from("1798843839"),
+  dataType: "0x12345678",
+  data: "0x00",
+};
+
+const orderErc1155InEthOut = [
+  orderErc1155InEthOutStruct.maker,
+  orderErc1155InEthOutStruct.makeAsset,
+  orderErc1155InEthOutStruct.taker,
+  orderErc1155InEthOutStruct.takeAsset,
+  orderErc1155InEthOutStruct.salt,
+  orderErc1155InEthOutStruct.start,
+  orderErc1155InEthOutStruct.end,
+  orderErc1155InEthOutStruct.dataType,
+  orderErc1155InEthOutStruct.data,
+];
+
+const orderERC20InERC1155OutStruct = {
+  maker: "0x000075B45Dff84C00Cf597d5C3E766108CeA0000".toLowerCase(),
+  makeAsset: {
+    assetType: {
+      assetClass: getAssetClass("ERC1155"),
+      data: ethers.utils.defaultAbiCoder.encode(
+        ["address", "uint256"],
+        ["0x00000000219ab540356cBB839Cbe05303d7705Fa", 2]
+      ),
+    },
+    value: "6",
+  },
+  taker: "0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8".toLowerCase(),
+  takeAsset: {
+    assetType: {
+      assetClass: getAssetClass("ERC20"),
+      data: ethers.utils.defaultAbiCoder.encode(
+        ["address"],
+        ["0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8"]
+      ),
+    },
+    value: "10000000000000000000",
+  },
+  salt: ethers.BigNumber.from(
+    "45118768841948961586167738353692277076075522015101619148498725069326976558864"
+  ),
+  start: ethers.BigNumber.from("1698643839"),
+  end: ethers.BigNumber.from("1798843839"),
+  dataType: "0x12345678",
+  data: "0x00",
+};
+
+const orderERC20InERC1155Out = [
+  orderERC20InERC1155OutStruct.maker,
+  orderERC20InERC1155OutStruct.makeAsset,
+  orderERC20InERC1155OutStruct.taker,
+  orderERC20InERC1155OutStruct.takeAsset,
+  orderERC20InERC1155OutStruct.salt,
+  orderERC20InERC1155OutStruct.start,
+  orderERC20InERC1155OutStruct.end,
+  orderERC20InERC1155OutStruct.dataType,
+  orderERC20InERC1155OutStruct.data,
+];
+
+const orderETHInERC721OutStruct = {
+  maker: "0x000075B45Dff84C00Cf597d5C3E766108CeA0000".toLowerCase(),
+  makeAsset: {
+    assetType: {
+      assetClass: getAssetClass("ERC721"),
+      data: ethers.utils.defaultAbiCoder.encode(
+        ["address", "uint256"],
+        ["0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", 10]
+      ),
+    },
+    value: "1",
+  },
+  taker: "0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8".toLowerCase(),
+  takeAsset: {
+    assetType: {
+      assetClass: getAssetClass("ETH"),
+      data: "0x00",
+    },
+    value: "100000000000000000",
+  },
+  salt: ethers.BigNumber.from(
+    "45118768841948961586167738353692277076075522015101619148498725069326976558864"
+  ),
+  start: ethers.BigNumber.from("1698643839"),
+  end: ethers.BigNumber.from("1798843839"),
+  dataType: "0x12345678",
+  data: "0x00",
+};
+
+const orderETHInERC721Out = [
+  orderETHInERC721OutStruct.maker,
+  orderETHInERC721OutStruct.makeAsset,
+  orderETHInERC721OutStruct.taker,
+  orderETHInERC721OutStruct.takeAsset,
+  orderETHInERC721OutStruct.salt,
+  orderETHInERC721OutStruct.start,
+  orderETHInERC721OutStruct.end,
+  orderETHInERC721OutStruct.dataType,
+  orderETHInERC721OutStruct.data,
+];
+
+Object.freeze(orderERC20InERC1155Out);
+Object.freeze(orderErc1155InEthOut);
+Object.freeze(orderETHInERC721Out);
+
+module.exports = {
+  orderErc1155InEthOut,
+  orderERC20InERC1155Out,
+  orderETHInERC721Out,
+  types,
+};
